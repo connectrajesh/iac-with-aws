c190089a12572ac97805d5731cf4f0e1caff5d42
Rajesh Muthusamy <m.rajesh2005@gmail.com>
2024-08-18 14:20:39 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,7 @@
 variable "aws_access_key" {
   description = "Access key for AWS provider"
   sensitive   = true
-}
- 
+} 
 variable "aws_secret_key" {
   description = "Secret key for AWS provider"
   sensitive   = true
